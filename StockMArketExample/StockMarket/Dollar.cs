76e8be4c7a87cc4d0370b3621248d2b2f00012d8
bgonzalez <barby850717@gmail.com>
2014-08-20 16:30:46 -0400

    Still failing but fixed compilation issues

---
+++ StockMArketExample/StockMarket/Dollar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Dollar
+    {
+        public int amount { get; set; }
+
+        public Dollar(int amount)
+        {
+        }
+
+        public void times(int multiplier)
+        {
+
+        }
+    }
+}
