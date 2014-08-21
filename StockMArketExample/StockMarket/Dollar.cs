d48120beb27d1c9444882c062126fc4d5680bcd6
bgonzalez <barby850717@gmail.com>
2014-08-21 16:12:47 -0400

    using the multiplier

--- StockMArketExample/StockMarket/Dollar.cs
+++ StockMArketExample/StockMarket/Dollar.cs
@@ -22,7 +22,7 @@ namespace StockMarket
         public Dollar times(int multiplier)
         {
             _amount *= multiplier;
-            return null;
+            return new Dollar(amount * multiplier);
         }
     }
 }
