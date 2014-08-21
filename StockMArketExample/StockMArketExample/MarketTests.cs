c96b77ca276071cb39045a8cc34a5f4fba168e9d
bgonzalez <barby850717@gmail.com>
2014-08-21 14:29:48 -0400

    Removing undesired side effects

--- StockMArketExample/StockMArketExample/MarketTests.cs
+++ StockMArketExample/StockMArketExample/MarketTests.cs
@@ -15,8 +15,10 @@ namespace StockMArketExample
         public void testMultiplication()
         {
             Dollar five = new Dollar(5);
-            five.times(2);
-            Assert.AreEqual(10, five.amount);
+            Dollar product = five.times(2);
+            Assert.AreEqual(10, product.amount);
+            product = five.times(3);
+            Assert.AreEqual(15, product.amount);
         }
     }
 }
